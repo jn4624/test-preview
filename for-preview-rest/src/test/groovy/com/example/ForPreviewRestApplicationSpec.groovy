132c4d88ba2468109107ccebc2aea3ff755b7694
LimJiNa <jn4624@gmail.com>
2025-09-02 18:35:55 +0900

    Rest API 테스트 - 기본 Application 테스트 클래스 네이밍 수정

--- for-preview-rest/src/test/groovy/com/example/ForPreviewRestApplicationSpec.groovy
+++ for-preview-rest/src/test/groovy/com/example/ForPreviewRestApplicationSpec.groovy
@@ -4,7 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
 @SpringBootTest
-class ForPreviewRestApplicationTests extends Specification {
+class ForPreviewRestApplicationSpec extends Specification {
 
     def "contextLoads"() {
         expect:
